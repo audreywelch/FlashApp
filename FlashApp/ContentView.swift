216c0f12a596c031b082677e5cfe4a8c3d2d37ca
audreywelch <welch.audreyl@gmail.com>
2020-04-01 16:13:57 -0700

    Added an Xcode project

---
+++ FlashApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashApp
+//
+//  Created by Audrey Welch on 4/1/20.
+//  Copyright © 2020 Audrey Welch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
