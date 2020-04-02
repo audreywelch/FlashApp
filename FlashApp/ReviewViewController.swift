119e1dd25a1f05a9a737645a249886474430b6d0
audreywelch <welch.audreyl@gmail.com>
2020-04-02 13:51:44 -0700

    Customized a plain UIButton

--- FlashApp/ReviewViewController.swift
+++ FlashApp/ReviewViewController.swift
@@ -14,10 +14,31 @@ class ReviewViewController: UIViewController {
         let rootView = UIView()
         rootView.backgroundColor = UIColor(named: "CardBackground")
         
+        // Configure the button
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(testTheButton(_:)), for: .primaryActionTriggered)
+        
+        button.setBackgroundImage(#imageLiteral(resourceName: "QuizButtonHighlighted"), for: .normal)
+        button.setBackgroundImage(#imageLiteral(resourceName: "QuizButtonNotChosen"), for: .highlighted)
+        
+        // Add to superview
+        rootView.addSubview(button)
+
+        // Constraints
+        button.widthAnchor.constraint(equalToConstant: 296).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 66).isActive = true
+        
+        button.centerXAnchor.constraint(equalTo: rootView.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: rootView.centerYAnchor).isActive = true
         
         
         // Assign the view we are creating to the view controller
         self.view = rootView
+    }
+    
+    @IBAction func testTheButton(_ sender: UIButton) {
+        print("The button was actually tapped")
     }
 
     override func viewDidLoad() {
